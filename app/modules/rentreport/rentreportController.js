d1359d01e31d15c9e736d7ea3687080b6c016d96
Sunita Karle <sunita@wtouch.in>
2015-05-02 19:14:18 +0530

    "invoice.html,
    payrentmodal.html,
    rentreport.html,
    modified"

--- app/modules/rentreport/rentreportController.js
+++ app/modules/rentreport/rentreportController.js
@@ -1,20 +1,46 @@
 'use strict';
 
 define(['app'], function (app) {
-    var injectParams = ['$scope','$rootScope','$injector','$location','$routeParams','dataService','upload','modalService','$notification'];
+    var injectParams = ['$scope','$rootScope','$injector','modalService'];
     
     // This is controller for this view
-	var rentreportController = function ($scope,$rootScope,$injector,$location,$routeParams,dataService,upload,modalService,$notification) {
+	var rentreportController = function ($scope,$rootScope,$injector,modalService) {
+		$scope.ok = function () {
+			$modalInstance.close();
+		};
+
+		$scope.cancel = function () {
+			$modalInstance.dismiss('cancel');
+		};
+		$scope.openRent = function (url) {
+			var modalDefaults = {
+					templateUrl: url,	
+					size : 'lg'
+			};
+			
+			modalService.showModal(modalDefaults).then(function (result) {
+				console.log("modal opened")	
+				$scope.ok = function(){
+					$modalOption.close("ok");
+				}
+			});
+		};
 		
-	$scope.open = function (size) {
-		var modalInstance = $modal.open({
-			templateUrl: 'payrentmodal.html',
-			controller: 'rentreportController',
-			size: size,
-		});
-		
-    console.log("rentreportController");
+		$scope.open= function (url) {
+			var modalDefaults = {
+					templateUrl: url,	
+					size : 'lg'
+			};
+			
+			modalService.showModal(modalDefaults).then(function (result) {
+				console.log("modal opened")	
+				$scope.ok = function(){
+					$modalOption.close("ok");
+				}
+			});
+		};
 	};
+		
 	// Inject controller's dependencies
 	rentreportController.$inject = injectParams;
 	// Register/apply controller dynamically
