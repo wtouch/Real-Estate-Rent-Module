b6202a8175221aaccbd585b0d2714ae815acd60f
vilas <vilas@wtouch.in>
2015-05-07 12:23:35 +0530

    some files deleted.

--- www/Server-api/testing/foreach.php
+++
@@ -1,9 +0,0 @@
-<?php
-$row = json_decode('{"id": "29","name":"John", "age":"36"}');
-
-foreach($row as $xx => $val)
-    {
-        echo $xx . ': ' . $val;
-        echo '<br>';
-    }
- ?>
--- www/Server-api/testing/show.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if($_SESSION['loggedIn'] == "true") {
-        echo "You are logged in";
-    } else {
-        die("You don't have permission to be here.");
-    }
- ?>
