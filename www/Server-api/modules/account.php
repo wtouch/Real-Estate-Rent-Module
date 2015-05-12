5f0bcc1c5d2c9f8f9dbefcb6adbcc2c9bc5e2a8b
Trupti Misal <trupti@wtouch.in>
2015-05-12 15:12:51 +0530

    breadcrumb.html
    menu.html
    menunew.html
    myaccount.html removed
    toolbar.html
    dashboard.html
    addexpenses.html
    addincome.html
    viewreport.html
    viewreportController.js
    account.php edited

--- www/Server-api/modules/account.php
+++ www/Server-api/modules/account.php
@@ -14,29 +14,19 @@
 			echo json_encode($data);
 			
 		}else{
+			
 			$like = array();
 			$userId = 0;
 			$limit[0] = $pageNo;
 			$limit[1] = $records;
 			$where = array();
-			if(isset($_GET['user_id'])) $userId = $_GET['user_id'];
+			if(isset($_GET['user_id'])) $where['user_id'] = $_GET['user_id'];
 			
-			/* if(isset($_GET['search']) && $_GET['search'] == true){
-				 
-				 (isset($_GET['template_name'])) ? $like['template_name'] = $_GET['template_name'] : "";
-			}
-			(isset($_GET['template_type'])) ? $where['template_type'] = $_GET['template_type'] : ""; */
-			(isset($_GET['status'])) ? $where['status'] = $_GET['status'] : "";
-		
-			$userCols['name'] = "name";
-			$userCols['username'] = "username";
-			$user = $db->getUsers($userId,$userCols);
+			$t0 = $db->setTable("account");
+			$db->setWhere($where, $t0);
+			$db->setWhere($like, "account", true);
 			$db->setLimit($limit);
-			$table = $db->setJoinString("INNER JOIN", "account", array("rent_id"=>$user.".id"));
-			$db->setWhere($where, $table);
-			$db->setWhere($like, $table, true);
-			$selectInnerJoinCols[0] = "*";
-			$db->setColumns($table, $selectInnerJoinCols);
+			
 			$data = $db->select();
 			echo json_encode($data);
 		}
--- www/modules/viewreport/viewreportController.js
+++ www/modules/viewreport/viewreportController.js
@@ -7,6 +7,14 @@ define(['app'], function (app) {
 	var viewreportController = function ($scope,$rootScope,$injector,modalService, $routeParams,$notification,dataService) {
 		
 		//global scope objects
+		$scope.maxSize = 5;
+		$scope.totalRecords = "";
+		$scope.currentPage = 1;
+		$scope.pageItems = 10;
+		$scope.numPages = "";		
+		$scope.alerts = [];
+		$scope.userInfo = {user_id : $rootScope.userDetails.id}; 
+		$scope.currentDate = dataService.currentDate;
 		$scope.currentDate = dataService.currentDate;
 		$scope.dates ={};
 		$scope.dates.date = $scope.currentDate;
@@ -32,6 +40,20 @@ define(['app'], function (app) {
 			});
 		};
 		
+		$scope.today = function() {
+			$scope.date = new Date();
+		};
+		$scope.open = function($event,rentdate){
+			$event.preventDefault();
+			$event.stopPropagation();
+			$scope.rentdate = ($scope.rentdate==true)?false:true;
+		};
+		$scope.opendate = function($event,selectDate){
+			$event.preventDefault();
+			$event.stopPropagation();
+			$scope.selectDate = ($scope.selectDate==true)?false:true;
+		};
+		
 		//function for Users list response
 		dataService.get("getmultiple/user/1/500", {status: 1, user_id : $rootScope.userDetails.id})
 		.then(function(response) {  
@@ -42,6 +64,8 @@ define(['app'], function (app) {
 				$notification[response.status]("Get Customers", response.message);
 			}
 		});
+		
+		
 		
 		$scope.postData = function(addincome) {
 				 dataService.post("post/account/addincome",addincome)
@@ -66,12 +90,24 @@ define(['app'], function (app) {
 				});   
 				console.log(addexpence);
 		}   
+		
+		dataService.get("/getmultiple/account/1/"+$scope.pageItems)
+			.then(function(response) {  //function for property response
+				if(response.status == 'success'){
+					$scope.account = response.data;
+					$scope.totalRecords = response.totalRecords;
+				}else{
+					$scope.account = {};
+					$scope.totalRecords = {};
+					$scope.alerts.push({type: response.status, msg: response.message});
+				}				
+			});
 			
 		
 		//function for Users list response
 		dataService.get("getmultiple/account/1/500", {status: 1, user_id : 1})
 		.then(function(response) {  
-		console.log(response)
+		//console.log(response)
 			/* if(response.status == 'success'){
 				$scope.rentreport = response.data;
 			}else{
